48f176000df8bb4a024d4ec845d3da55a346b56b
KhBayazidAhmed <khandakerbayazidahmed@gmail.com>
2024-05-25 17:56:34 +0600

    adding the mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Logo from "../../public/Images/logo.svg";
 import Link from "next/link";
 import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { CgMenuGridO } from "react-icons/cg";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
@@ -40,12 +40,12 @@ const Header = () => {
         <Image src={Logo} alt="WebDevBiz" width={150} />
       </Link>
       <div onClick={showNavInMobile} className="sm:hidden cursor-pointer">
-        <GiHamburgerMenu size={35} />
+        <CgMenuGridO size={30} />
       </div>
       <div
         className={
           mobileMenu
-            ? `flex absolute items-center text-white flex-col left-0 top-[85px] pt-1 bg-gray-800  w-full`
+            ? `flex absolute items-center text-white flex-col left-0 top-[85px] pt-1 bg-[#181826] border-b border-gray-700 shadow-2xl w-full`
             : `hidden sm:flex gap-6`
         }
       >
